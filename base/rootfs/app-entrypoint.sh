43c852fcba59feed15e1731eaece57a7f9ccd514
Richard Ting <richard.tin@gmail.com>
2021-07-22 16:51:44 +0800

    feat: create 'gradle.properties' in the GRADLE_USER_HOME

--- base/rootfs/app-entrypoint.sh
+++ base/rootfs/app-entrypoint.sh
@@ -5,20 +5,24 @@ if [ -z ${HTTP_PROXY+x} ]; then
 else
     echo "HTTP_PROXY is set to '$HTTP_PROXY'"
 
-# Get system proxy setting
-PROXY_REGEX='http:\/\/(.*):([0-9]+)'
+    # Get system proxy setting
+    PROXY_REGEX='http:\/\/(.*):([0-9]+)'
 
-[[ $HTTP_PROXY =~ $PROXY_REGEX ]] && \
-proxyHost=${BASH_REMATCH[1]} && \
-proxyPort=${BASH_REMATCH[2]}
+    [[ $HTTP_PROXY =~ $PROXY_REGEX ]] && \
+    proxyHost=${BASH_REMATCH[1]} && \
+    proxyPort=${BASH_REMATCH[2]}
 
-# Create gradle.properties
-mkdir -p ~/.gradle
-echo -e "systemProp.http.proxyHost=${proxyHost}\n"\
+    # Create gradle.properties
+    if [ -z ${GRADLE_USER_HOME+x} ]; then
+        GRADLE_USER_HOME=${HOME}/.gradle
+    fi
+    mkdir -p ${GRADLE_USER_HOME}
+
+    echo -e "systemProp.http.proxyHost=${proxyHost}\n"\
 "systemProp.https.proxyHost=${proxyHost}\n"\
 "systemProp.https.proxyPort=${proxyPort}\n"\
 "systemProp.http.proxyPort=${proxyPort}\n"\
-"systemProp.jdk.http.auth.tunneling.disabledSchemes=\"\"" > ~/.gradle/gradle.properties
+"systemProp.jdk.http.auth.tunneling.disabledSchemes=\"\"" > ${GRADLE_USER_HOME}/gradle.properties
 
 fi
 
